621aa79852a3db6b8c5665f7c3f3507218aa6785
frisqia fatmalaningsih <frifani123@gmail.com>
2024-05-26 14:25:39 +0700

    tes

--- Main.py
+++ Main.py
@@ -77,4 +77,8 @@ print("data:", data_c_double, ", bertipe:", type(data_c_double))
 data_int = 9
 print("data =", data_int, "type =", type(data_int))
 data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
 print("data =", data_float, "type =", type(data_float))
+print("data =", data_str, "type =", type(data_str))
+print("data =", data_bool, "type =", type(data_bool))
